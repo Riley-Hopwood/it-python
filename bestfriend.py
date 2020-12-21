af6edac1c993453e0d4ec22958672de73ee717f5
Riley-hopwood <hopwoodriley0@gmail.com>
2020-12-21 09:59:00 -0500

    Adds best friend maker

---
+++ bestfriend.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+print('+-----------------+')
+print('|Best friend maker|')
+print('|    By Riley     |')
+print('+-----------------+')
+print('')
+print("Hi. I'm the computer.")
+
+name = input('What is your name?')
+color = input(f'Hi {name}. What is your favorite color?')
+print(f'No way! My favorite color is also {color}!')
+
+movie = input('what is your favorite movie?')
+print(f'What!?! My favorite movie is also {movie}')
+
+game = input('what is your faorite game')
+print(f'Get out of here! My favorite game is {game}, too')
+print('We like the same things! We should be friends!')
