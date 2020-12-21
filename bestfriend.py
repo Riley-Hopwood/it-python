79dab8eaac70ec2eb235b89ab1b17f46dc6fee23
Riley-hopwood <hopwoodriley0@gmail.com>
2020-12-21 10:20:47 -0500

    Adds go-agains

--- bestfriend.py
+++ bestfriend.py
@@ -7,13 +7,19 @@ print('+-----------------+')
 print('')
 print("Hi. I'm the computer.")
 
-name = input('What is your name?')
-color = input(f'Hi {name}. What is your favorite color?')
-print(f'No way! My favorite color is also {color}!')
+while True:
+    name = input('What is your name?')
+    color = input(f'Hi {name}. What is your favorite color?')
+    print(f'No way! My favorite color is also {color}!')
 
-movie = input('what is your favorite movie?')
-print(f'What!?! My favorite movie is also {movie}')
+    movie = input('what is your favorite movie?')
+    print(f'What!?! My favorite movie is also {movie}')
 
-game = input('what is your faorite game')
-print(f'Get out of here! My favorite game is {game}, too')
-print('We like the same things! We should be friends!')
+    game = input('what is your faorite game')
+    print(f'Get out of here! My favorite game is {game}, too')
+    print('We like the same things! We should be friends!')
+    print('')
+    response = input('would you like to go again? (Y/n) ')
+    
+    if response.lower() in ['n','no', 'nope', 'nu-uh']:
+        break
