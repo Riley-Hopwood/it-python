eb2092bba117f3fa34fc2cd97d5b4ebe840b4b1d
Riley-hopwood <hopwoodriley0@gmail.com>
2020-12-21 09:32:37 -0500

    Adds questions to hellow wolrd

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 
 print('+--------------+')
-print('HELLO WORLD |')
-print('By Riley    |')
+print('|  HELLO WORLD |')
+print('|   By Riley   |')
 print('+--------------+')
 print('')
 
-print('Hello World')
+name = input('What is your name?')
+age = input('what is your age? ')
+if age.isnumeric():
+    age = int(age)
+    print(f'Hello {name}. You are {age} years old')
+    print(f'Next year, you will be {age+1} years old')
+else:
+    print("you didn't type a valid number")
